d125d5469dfcc84e0d031ff13f10abe336f375e2
Tony Rönnqvist <tonyr@kth.se>
2020-02-13 17:18:05 +0100

    [FIX] - Fixed the print issues according to issue #31 Fixed the print issue in TestSuite and in CoverageTool. Also refactord some code to reflect these changes, removed code that was redundant. close #31

--- core/src/test/java/com/google/zxing/TestSuite.java
+++ core/src/test/java/com/google/zxing/TestSuite.java
@@ -1,7 +1,10 @@
 package com.google.zxing;
 
+import com.google.zxing.common.AbstractBlackBoxTestCase;
 import com.google.zxing.oned.Code39ExtendedBlackBox2TestCase;
 import com.google.zxing.oned.Code39ExtendedModeTestCase;
+import com.google.zxing.oned.rss.RSS14BlackBox1TestCase;
+import com.google.zxing.oned.rss.RSS14BlackBox2TestCase;
 import junit.framework.TestCase;
 
 import org.junit.AfterClass;
@@ -11,7 +14,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({Code39ExtendedModeTestCase.class, Code39ExtendedBlackBox2TestCase.class})
+@SuiteClasses({Code39ExtendedModeTestCase.class, Code39ExtendedBlackBox2TestCase.class, RSS14BlackBox1TestCase.class,
+  RSS14BlackBox2TestCase.class})
 public class TestSuite extends TestCase {
   /**
    * One time setup before the tests are run.
@@ -19,13 +23,16 @@ public class TestSuite extends TestCase {
   @BeforeClass
   public static void setup() {
     CoverageTool2000.initCoverageMatrix(0, 24);
-    CoverageTool2000.initCoverageMatrix(1, 18);
+    CoverageTool2000.initCoverageMatrix(1, 13);
+    //CoverageTool2000.initCoverageMatrix(3, 25);
   }
   /**
    * One time teardown after all the tests are run.
    */
   @AfterClass
   public static void teardown() {
-    System.out.println(CoverageTool2000.checkCoverage(0));
+    //System.out.println(CoverageTool2000.checkCoverage(0));
+    //System.out.println(CoverageTool2000.checkCoverage(1));
+    CoverageTool2000.checkAllCoverage(2);
   }
 }
