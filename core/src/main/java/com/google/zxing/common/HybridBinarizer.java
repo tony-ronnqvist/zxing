2843b84bb9a8ea1ac3b574ad9fb2339ce9942417
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-02-13 15:32:23 +0100

    [REFACTOR] Refactored method calculateBlackPoints, split it up into three methods to reduce complexity close #18

--- core/src/main/java/com/google/zxing/common/HybridBinarizer.java
+++ core/src/main/java/com/google/zxing/common/HybridBinarizer.java
@@ -158,80 +158,98 @@ public final class HybridBinarizer extends GlobalHistogramBinarizer {
    * See the following thread for a discussion of this algorithm:
    *  http://groups.google.com/group/zxing/browse_thread/thread/d06efa2c35a7ddc0
    */
-  private static int[][] calculateBlackPoints(byte[] luminances,
-                                              int subWidth,
-                                              int subHeight,
-                                              int width,
-                                              int height) {
-    int maxYOffset = height - BLOCK_SIZE;
-    int maxXOffset = width - BLOCK_SIZE;
-    int[][] blackPoints = new int[subHeight][subWidth];
-    for (int y = 0; y < subHeight; y++) {
-      int yoffset = y << BLOCK_SIZE_POWER;
-      if (yoffset > maxYOffset) {
-        yoffset = maxYOffset;
+  private static int[][] calculateBlackPoints(byte[] luminances, int subWidth, int subHeight, int width, int height) {
+  int maxYOffset = height - BLOCK_SIZE;
+  int maxXOffset = width - BLOCK_SIZE;
+  int[][] blackPoints = new int[subHeight][subWidth];
+  for (int y = 0; y < subHeight; y++) {
+    int yoffset = y << BLOCK_SIZE_POWER;
+    if (yoffset > maxYOffset) {
+      yoffset = maxYOffset;
+    }
+    for (int x = 0; x < subWidth; x++) {
+      int xoffset = x << BLOCK_SIZE_POWER;
+      if (xoffset > maxXOffset) {
+        xoffset = maxXOffset;
       }
-      for (int x = 0; x < subWidth; x++) {
-        int xoffset = x << BLOCK_SIZE_POWER;
-        if (xoffset > maxXOffset) {
-          xoffset = maxXOffset;
-        }
-        int sum = 0;
-        int min = 0xFF;
-        int max = 0;
-        for (int yy = 0, offset = yoffset * width + xoffset; yy < BLOCK_SIZE; yy++, offset += width) {
-          for (int xx = 0; xx < BLOCK_SIZE; xx++) {
-            int pixel = luminances[offset + xx] & 0xFF;
-            sum += pixel;
-            // still looking for good contrast
-            if (pixel < min) {
-              min = pixel;
-            }
-            if (pixel > max) {
-              max = pixel;
-            }
-          }
-          // short-circuit min/max tests once dynamic range is met
-          if (max - min > MIN_DYNAMIC_RANGE) {
-            // finish the rest of the rows quickly
-            for (yy++, offset += width; yy < BLOCK_SIZE; yy++, offset += width) {
-              for (int xx = 0; xx < BLOCK_SIZE; xx++) {
-                sum += luminances[offset + xx] & 0xFF;
-              }
-            }
-          }
-        }
+      int sum = 0;
+      int min = 0xFF;
+      int max = 0;
+      for (int yy = 0, offset = yoffset * width + xoffset; yy < BLOCK_SIZE; yy++, offset += width) {
+        int[] values = calculateRowContrast(min, max, sum, offset, luminances);
+        min = values[0];
+        max = values[1];
+        sum = dynamicQuickTraverse(yy, min, max, values[2], width, offset, luminances);
+      }
 
-        // The default estimate is the average of the values in the block.
-        int average = sum >> (BLOCK_SIZE_POWER * 2);
-        if (max - min <= MIN_DYNAMIC_RANGE) {
-          // If variation within the block is low, assume this is a block with only light or only
-          // dark pixels. In that case we do not want to use the average, as it would divide this
-          // low contrast area into black and white pixels, essentially creating data out of noise.
-          //
-          // The default assumption is that the block is light/background. Since no estimate for
-          // the level of dark pixels exists locally, use half the min for the block.
-          average = min / 2;
+      // The default estimate is the average of the values in the block.
+      int average = sum >> (BLOCK_SIZE_POWER * 2);
+      if (max - min <= MIN_DYNAMIC_RANGE) {
+        // If variation within the block is low, assume this is a block with only light
+        // or only
+        // dark pixels. In that case we do not want to use the average, as it would
+        // divide this
+        // low contrast area into black and white pixels, essentially creating data out
+        // of noise.
+        //
+        // The default assumption is that the block is light/background. Since no
+        // estimate for
+        // the level of dark pixels exists locally, use half the min for the block.
+        average = min / 2;
 
-          if (y > 0 && x > 0) {
-            // Correct the "white background" assumption for blocks that have neighbors by comparing
-            // the pixels in this block to the previously calculated black points. This is based on
-            // the fact that dark barcode symbology is always surrounded by some amount of light
-            // background for which reasonable black point estimates were made. The bp estimated at
-            // the boundaries is used for the interior.
+        if (y > 0 && x > 0) {
+          // Correct the "white background" assumption for blocks that have neighbors by
+          // comparing
+          // the pixels in this block to the previously calculated black points. This is
+          // based on
+          // the fact that dark barcode symbology is always surrounded by some amount of
+          // light
+          // background for which reasonable black point estimates were made. The bp
+          // estimated at
+          // the boundaries is used for the interior.
 
-            // The (min < bp) is arbitrary but works better than other heuristics that were tried.
-            int averageNeighborBlackPoint =
-                (blackPoints[y - 1][x] + (2 * blackPoints[y][x - 1]) + blackPoints[y - 1][x - 1]) / 4;
-            if (min < averageNeighborBlackPoint) {
-              average = averageNeighborBlackPoint;
-            }
+          // The (min < bp) is arbitrary but works better than other heuristics that were
+          // tried.
+          int averageNeighborBlackPoint = (blackPoints[y - 1][x] + (2 * blackPoints[y][x - 1])
+              + blackPoints[y - 1][x - 1]) / 4;
+          if (min < averageNeighborBlackPoint) {
+            average = averageNeighborBlackPoint;
           }
         }
-        blackPoints[y][x] = average;
+      }
+      blackPoints[y][x] = average;
+    }
+  }
+  return blackPoints;
+}
+
+private static int[] calculateRowContrast(int min, int max, int sum, int offset, byte[] luminances) {
+  for (int xx = 0; xx < BLOCK_SIZE; xx++) {
+    int pixel = luminances[offset + xx] & 0xFF;
+    sum += pixel;
+    // still looking for good contrast
+    if (pixel < min) {
+      min = pixel;
+    }
+    if (pixel > max) {
+      max = pixel;
+    }
+  }
+  return new int[] { min, max, sum };
+
+}
+
+private static int dynamicQuickTraverse(int yy, int min, int max, int sum, int width, int offset, byte[] luminances) {
+  // short-circuit min/max tests once dynamic range is met
+  if (max - min > MIN_DYNAMIC_RANGE) {
+    // finish the rest of the rows quickly
+    for (yy++, offset += width; yy < BLOCK_SIZE; yy++, offset += width) {
+      for (int xx = 0; xx < BLOCK_SIZE; xx++) {
+        sum += luminances[offset + xx] & 0xFF;
       }
     }
-    return blackPoints;
   }
+  return sum;
+}
 
 }
