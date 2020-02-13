8825352f1eb6837fd215e0535d965223937a63e4
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-02-13 15:45:10 +0100

    [ADD] Added test of coverage in calculateBlackPoints method close #20

--- core/src/test/java/com/google/zxing/TestSuite.java
+++ core/src/test/java/com/google/zxing/TestSuite.java
@@ -20,6 +20,7 @@ public class TestSuite extends TestCase {
   public static void setup() {
     CoverageTool2000.initCoverageMatrix(0, 24);
     CoverageTool2000.initCoverageMatrix(1, 18);
+    CoverageTool2000.initCoverageMatrix(4, 10);
   }
   /**
    * One time teardown after all the tests are run.
