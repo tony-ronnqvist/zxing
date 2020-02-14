34e0188948fe8d9f4cbed559a6ce2f2902fb4535
ChristinaPetersson <chrpete@kth.se>
2020-02-14 13:19:52 +0100

    [ADD] - added the class shell for added Tests #39

---
+++ core/src/test/java/com/google/zxing/TestCoverage.java
@@ -0,0 +1,17 @@
+package com.google.zxing;
+
+
+import com.google.zxing.oned.CodaBarWriter;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Added test two increase the coverage on the chosen 10 - methods.
+ */
+public class TestCoverage extends Assert {
+
+
+  // @Test
+  // Put in your test methods
+
+}
